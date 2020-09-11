68518d8d5199b90e602111fbd9470069098c44ab
Shubham Agarwal <shubham.ag1038@gmail.com>
2020-09-12 01:20:15 +0530

    initial version

--- docs/main-es2015.js
+++ docs/main-es2015.js
@@ -524,7 +524,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 function HttpLoaderFactory(httpClient) {
-    return new _ngx_translate_http_loader__WEBPACK_IMPORTED_MODULE_6__["TranslateHttpLoader"](httpClient, '/projectManagement/assests/i18n');
+    return new _ngx_translate_http_loader__WEBPACK_IMPORTED_MODULE_6__["TranslateHttpLoader"](httpClient, '/projectManagement/assests/i18n/');
 }
 class AppModule {
 }
